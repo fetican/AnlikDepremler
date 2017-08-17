38c3fb7b2db120a62760cd75e859a880dd8a2ae2
Feti Can Kirazcı <feticankirazci@gmail.com>
2017-08-18 01:03:30 +0300

    Important Earth Quakes tab added

--- app/src/main/java/com/feticankirazci/anlikdepremler/MainActivity.java
+++ app/src/main/java/com/feticankirazci/anlikdepremler/MainActivity.java
@@ -1,21 +1,25 @@
 package com.feticankirazci.anlikdepremler;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.feticankirazci.anlikdepremler.models.EarthQuakesList;
+import com.feticankirazci.anlikdepremler.models.ImportantEarthQuakesList;
 import com.feticankirazci.anlikdepremler.network.Factory;
+import com.feticankirazci.anlikdepremler.network.ImportantFactory;
+import com.feticankirazci.anlikdepremler.services.ImportantServiceEvent;
 import com.feticankirazci.anlikdepremler.services.ResponseServiceEvent;
-import com.feticankirazci.anlikdepremler.ui.ContactsFragment;
 import com.feticankirazci.anlikdepremler.ui.EarthQuakesListFragment;
+import com.feticankirazci.anlikdepremler.ui.ImportantEarthQuakesFragment;
 import com.feticankirazci.anlikdepremler.ui.MainMenuFragment;
 
 import org.greenrobot.eventbus.EventBus;
@@ -28,10 +32,14 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.bottomNavigationView)BottomNavigationView mBottomNavigationView;
-    @BindView(R.id.container)FrameLayout mContainer;
+    @BindView(R.id.bottomNavigationView)
+    BottomNavigationView mBottomNavigationView;
+    @BindView(R.id.container)
+    FrameLayout mContainer;
     private FragmentManager fm;
     private FragmentTransaction ft;
+//    private ArrayList<Contacts> mContactsList;
+//    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         getEarthQuakeList();
+        getImportantEarthQuakesList();
+//        showContacts();
         setBottomNavigationBar();
         initial();
     }
@@ -48,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment selectedFragment = null;
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.main_menu:
                         getEarthQuakeList();
                         selectedFragment = MainMenuFragment.newInstance();
@@ -58,7 +68,8 @@ public class MainActivity extends AppCompatActivity {
                         selectedFragment = EarthQuakesListFragment.newInstance();
                         break;
                     case R.id.contacts:
-                        selectedFragment = ContactsFragment.newInstance();
+                        getImportantEarthQuakesList();
+                        selectedFragment = ImportantEarthQuakesFragment.newInstance();
                         break;
                 }
                 fm = getSupportFragmentManager();
@@ -73,20 +84,77 @@ public class MainActivity extends AppCompatActivity {
     private void initial() {
         fm = getSupportFragmentManager();
         ft = fm.beginTransaction();
-        ft.replace(R.id.container,MainMenuFragment.newInstance());
+        ft.replace(R.id.container, MainMenuFragment.newInstance());
         ft.commit();
     }
 
-    private void getEarthQuakeList(){
+    private void getEarthQuakeList() {
         Factory.getInstance().getEarthQuakesList().enqueue(new Callback<EarthQuakesList>() {
             @Override
             public void onResponse(Call<EarthQuakesList> call, Response<EarthQuakesList> response) {
-                EventBus.getDefault().postSticky(new ResponseServiceEvent(null,response));
+                EventBus.getDefault().postSticky(new ResponseServiceEvent(null, response));
             }
 
             @Override
             public void onFailure(Call<EarthQuakesList> call, Throwable t) {
+                Log.d(MainActivity.class.getName(),"Failure",t);
+                Toast.makeText(MainActivity.this,t.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
-}
+
+    private void getImportantEarthQuakesList(){
+        ImportantFactory.getInstance().getImportantEarthQuakesList().enqueue(new Callback<ImportantEarthQuakesList>() {
+            @Override
+            public void onResponse(Call<ImportantEarthQuakesList> call, Response<ImportantEarthQuakesList> response) {
+                EventBus.getDefault().postSticky(new ImportantServiceEvent(null, response));
+            }
+
+            @Override
+            public void onFailure(Call<ImportantEarthQuakesList> call, Throwable t) {
+                Log.d(MainActivity.class.getName(),"Failure",t);
+            }
+        });
+    }
+
+//    private void showContacts(){
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+//            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+//        } else { getContactList(); }
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                // Permission is granted
+//                showContacts();
+//            } else {
+//                Toast.makeText(this, "İzin vermediğiniz takdirde Kişiler'inize ulaşamayız.", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
+//
+//    private void getContactList() {
+//
+//            mContactsList = new ArrayList<>();
+//            ContentResolver resolver = getContentResolver();
+//            Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+//
+//            while (cursor.moveToNext()) {
+//                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+//                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+//
+//                Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+//                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+//
+//                while (phoneCursor.moveToNext()) {
+//                    String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+//                    mContactsList.add(new Contacts(name, phoneNumber));
+//                }
+//            }
+//            EventBus.getDefault().postSticky(new ContactsListEvent(null, mContactsList));
+//        }
+    }
+
